734d37ae3fa68fbcdb46b75a4f6fb7ec0c5425f2
SergiusZurawski <sergi.zhuravski@gmail.com>
2017-10-28 23:41:59 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetTestFrameWorkNuget
+{
+    class Program
+    {
+        // static void Main(string[] args)
+        // {
+        //     Console.WriteLine("Hello World!");
+        // }
+    }
+}
